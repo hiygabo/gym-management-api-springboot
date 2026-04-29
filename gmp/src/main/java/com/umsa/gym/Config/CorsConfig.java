52d8c243f9511501bac0e2d0731fa179f446032a
hiygabo <gaboandiaalave@gmail.com>
2026-04-28 23:00:40 -0400

    Cors Config add

---
+++ gmp/src/main/java/com/umsa/gym/Config/CorsConfig.java
@@ -0,0 +1,11 @@
+package com.umsa.gym.Config;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+public class CorsConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+    
+}
