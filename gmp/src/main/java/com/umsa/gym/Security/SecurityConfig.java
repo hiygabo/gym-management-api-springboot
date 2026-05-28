8bb4b5b1f91c1b5c96553196d65f51ba52354bcd
hiygabo <gaboandiaalave@gmail.com>
2026-05-28 03:20:52 -0400

    delete(comments) comments

--- gmp/src/main/java/com/umsa/gym/Security/SecurityConfig.java
+++ gmp/src/main/java/com/umsa/gym/Security/SecurityConfig.java
@@ -28,29 +28,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            // 1. Configuramos CORS para que React (puerto 5173) pueda comunicarse con Java sin bloqueos
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-            // 2. Desactivamos CSRF porque usaremos Tokens (esto es estándar en APIs REST)
             .csrf(csrf -> csrf.disable())
-            // 3. ¡LAS REGLAS DE LA PUERTA!
             .authorizeHttpRequests(auth -> auth
-                // Permisos públicos (permitAll): Cualquiera puede entrar sin token
-                .requestMatchers("/api/auth/**").permitAll() // Aquí pondremos el endpoint para hacer Login
-                .requestMatchers("/api/asistencia/marcar").permitAll() // El kiosko de recepción
-                .requestMatchers("/api/registros/**").permitAll() // ¡Ajusta esta ruta a la de tu formulario de inscripción de clientes!
-                .requestMatchers("/api/planes/**").permitAll() // <-- ¡AÑADE ESTA LÍNEA!
-                // Todo el resto de la aplicación (Clientes, Entrenadores, etc.) está BAJO LLAVE
+                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/api/asistencia/marcar").permitAll()
+                .requestMatchers("/api/registros/**").permitAll()
+                .requestMatchers("/api/planes/**").permitAll()
                 .anyRequest().authenticated()
             )
-            // 4. Le decimos que no guarde sesiones en memoria, todo será a través del Token
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            // 5. Ponemos a nuestro Guardia de Seguridad (Filtro) antes de que la petición llegue al controlador
             .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
-    // Configuración exacta para permitirle el paso a tu frontend de React
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
@@ -62,13 +54,11 @@ public class SecurityConfig {
         return source;
     }
 
-    // El encriptador de contraseñas. Nunca guardaremos "1234" literal en Oracle.
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    // El motor que verifica si el usuario y contraseña son correctos
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
